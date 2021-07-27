9a2f8a93515584a4ec119a43fd2b48c3fe8c9798
MickPhysics <michelangelo.mazzitelli@gmail.com>
2021-07-27 11:22:13 +0200

    Clean Application

--- CorsiStudenti/src/main/java/com/s2e/app/CorsiStudentiApplication.java
+++ CorsiStudenti/src/main/java/com/s2e/app/CorsiStudentiApplication.java
@@ -3,25 +3,14 @@ package com.s2e.app;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.ConfigurableApplicationContext;
-//
-//import com.s2e.app.repository.CourseRepository;
-//import com.s2e.app.repository.StudentRepository;
 
 @SpringBootApplication
 public class CorsiStudentiApplication {
 
 	public static void main(String[] args) {
-		// voglio fare qualche test
-		// Voglio il context
-		// ConfigurableApplicationContext configurableApplicationContext =
+
 		SpringApplication.run(CorsiStudentiApplication.class, args);
 
-//		StudentRepository studentRepo = 
-//				configurableApplicationContext.getBean(StudentRepository.class);
-//				
-//		CourseRepository courseRepo = 
-//				configurableApplicationContext.getBean(CourseRepository.class);
 	}
 
 }
