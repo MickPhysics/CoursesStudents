96b85f5482f9ed4910d3f496582f405bd17c998a
MickPhysics <michelangelo.mazzitelli@gmail.com>
2021-07-27 11:13:20 +0200

    after merge

--- CorsiStudenti/src/test/java/com/s2e/app/CorsiStudentiApplicationTests.java
+++ CorsiStudenti/src/test/java/com/s2e/app/CorsiStudentiApplicationTests.java
@@ -11,63 +11,50 @@ package com.s2e.app;
 //import org.apache.http.HttpStatus;
 //import org.apache.http.client.ClientProtocolException;
 //import org.apache.http.client.methods.HttpGet;
-//import org.apache.http.client.methods.HttpPost;
 //import org.apache.http.client.methods.HttpUriRequest;
-//import org.apache.http.client.methods.RequestBuilder;
-//import org.apache.http.entity.ContentType;
-//import org.apache.http.entity.StringEntity;
 //import org.apache.http.impl.client.HttpClientBuilder;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
-//import com.fasterxml.jackson.core.JsonProcessingException;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.google.gson.Gson;
-//import com.s2e.app.model.Student;
-//import com.s2e.app.repository.StudentRepository;
-//
 //import static org.junit.Assert.*;
 
-@SpringBootTest
-class ApplicationTests {
 
-//	@Autowired
-//	StudentRepository studentRepo;
-//
-//	@Test
+@SpringBootTest
+class CorsiStudentiApplicationTests {
+    @LocalServerPort
+	private int port;
+	@Test
+	void contextLoads() {
+	}
+	
+	/*
+	 * Test di un servizio Rest
+	 */
+	
+//	@Test 
 //	void givenStudents_studentsReceived_status200() throws ClientProtocolException, IOException {
-//
-//		// compilo la richiesta http
-//		HttpUriRequest request = new HttpGet("http://localhost:4202/students");
-//
-//		// eseguo la richiesta http
+//		
+//		System.out.println("test partito");
+//		// Scrivo la richiesta http
+//		HttpUriRequest request = new HttpGet("http://localhost:4202/students/");
 //		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-//
-//		// controllo che lo stato sia 200
+//		// 200 response test
 //		assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_OK));
-//
+//		
+//		System.out.println("test eseguito");
 //	}
-//
+//	
 //	@Test
-//	void givenCourse_coursesReceived_status200() throws ClientProtocolException, IOException {
-//		HttpUriRequest request = new HttpGet("http://localhost:4202/courses");
+//	void givenStudentsDelete_deleteById() throws ClientProtocolException, IOException {
+//		
+//		int idTest = 1;
+//		HttpUriRequest request = new HttpGet("http://localhost:4202/students/"+idTest);
 //		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
 //		assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_OK));
 //	}
-//
-//	@Test
-//	void postNewStudent() throws ClientProtocolException, IOException {
-//		Student student = new Student(0, "Maria", "Luce", "maria.luce@mail.com");
-//		Gson gson = new Gson();
-//		String json = gson.toJson(student);
-//		HttpUriRequest request = RequestBuilder.create("POST").setUri("http://localhost:4202/students")
-//				.setEntity(new StringEntity(json, ContentType.APPLICATION_JSON)).build();
-//		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-//		Student s = studentRepo.getById(0);
-//		assertNotNull(s);
-//		assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_OK));
-//
-//	}
+	
+	
+	
 
 }
